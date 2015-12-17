4262fb190d134f7cf1c958715090bcee879deddf
sangcomz <sangcomz@naver.com>
2015-12-17 18:00:52 +0900

    add

--- app/src/main/java/xyz/sangcomz/sangcomz_n_study/ui/main/MainActivity.java
+++ app/src/main/java/xyz/sangcomz/sangcomz_n_study/ui/main/MainActivity.java
@@ -1,7 +1,9 @@
-package xyz.sangcomz.sangcomz_n_study;
+package xyz.sangcomz.sangcomz_n_study.ui.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import xyz.sangcomz.sangcomz_n_study.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/xyz/sangcomz/sangcomz_n_study/ui/splash/SplashActivity.java
@@ -0,0 +1,15 @@
+package xyz.sangcomz.sangcomz_n_study.ui.splash;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import xyz.sangcomz.sangcomz_n_study.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
