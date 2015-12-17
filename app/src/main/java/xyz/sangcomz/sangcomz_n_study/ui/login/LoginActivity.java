60248185a6c3b0cf5b16a32a7a3854678a269bf4
sangcomz <sangcomz@naver.com>
2015-12-17 18:40:34 +0900

    no message

---
+++ app/src/main/java/xyz/sangcomz/sangcomz_n_study/ui/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package xyz.sangcomz.sangcomz_n_study.ui.login;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import xyz.sangcomz.sangcomz_n_study.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
